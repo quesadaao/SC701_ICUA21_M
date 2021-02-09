010fc2da29029d99448c06f63aeab86768f7d650
Oscar.Quesada <quesadaao@hotmail.com>
2021-02-08 20:51:09 -0600

    primer commit de codigo, falta la capa API

---
+++ Codigo_P1/Codigo_Clase/Solution_P1/Solution.DO/Interfases/ICRUD.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution.DO.Interfases
+{
+    public interface ICRUD<T>
+    {
+        void Insert(T t);
+        void Update(T t);
+        void Delete(T t);
+        IEnumerable<T> Getll();
+        T GetOneById(int id);
+
+    }
+}
